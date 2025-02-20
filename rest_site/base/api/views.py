b6dc6865237d46feb62e31ececba8edd7e5a6da1
Hitesh <hiteshna@atrihub.io>
2025-02-20 11:25:47 -0800

    feat(add): Added custom api view to retrieve data [ICTRAINING-1848]

--- rest_site/base/api/views.py
+++ rest_site/base/api/views.py
@@ -3,6 +3,5 @@ from django.http import JsonResponse
 def routes(request):
     routes = [
         'GET /api/students',
-        'GET /api/students/:id',
     ]
     return JsonResponse(routes, safe=False)
