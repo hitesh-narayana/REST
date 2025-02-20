d43a854302e15130ee8da98ce0b0ecfc0c9cd869
Hitesh <hiteshna@atrihub.io>
2025-02-20 10:49:54 -0800

    feat(add): Setting up django project

--- rest_site/base/views.py
+++ rest_site/base/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from .models import Student
 
 # Create your views here.
+
+def home(request):
+    students = Student.objects.all()
+    context  = {'students':students}
+    return render(request,'home.html',context)
--- rest_site/rest_site/settings.py
+++ rest_site/rest_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'rest_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'rest_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest_db',  
+        'USER': 'hitesh',       
+        'PASSWORD': 'hnarayana', 
+        'HOST': 'localhost',      
+        'PORT': '5432',          
     }
 }
 
