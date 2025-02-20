45246b7e954486cb8736317d924657098a1cf5bf
Hitesh <hiteshna@atrihub.io>
2025-02-20 14:41:16 -0800

    feat(add): Serialize fields [ICTRAINING-1850]

--- rest_site/rest_site/settings.py
+++ rest_site/rest_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     'base.apps.BaseConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'rest_site.urls'
@@ -131,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
