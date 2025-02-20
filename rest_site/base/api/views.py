9d5aec16dede6519145e38f6a7a419dc642cdb8d
Hitesh <hiteshna@atrihub.io>
2025-02-20 11:32:01 -0800

    feat(add): install django framework and apply api_view decorator [ICTRAINING-1848]

--- rest_site/base/api/views.py
+++ rest_site/base/api/views.py
@@ -1,7 +1,12 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET'])
 def routes(request):
     routes = [
+        'GET /api/',
         'GET /api/students',
+
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
