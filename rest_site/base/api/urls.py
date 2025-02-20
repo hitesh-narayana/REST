54ac347cf8e923eaf7b1d84dae8d5eaf516330f4
Hitesh <hiteshna@atrihub.io>
2025-02-20 11:35:01 -0800

    type(error): JSON object not serialized [ICTRAINING-1849]

--- rest_site/base/api/urls.py
+++ rest_site/base/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.routes,name='routes'),
+    path('students/',views.getStudents,name='getStudents'),
 ]
--- rest_site/base/api/views.py
+++ rest_site/base/api/views.py
@@ -1,12 +1,17 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Student
 
 @api_view(['GET'])
 def routes(request):
     routes = [
         'GET /api/',
         'GET /api/students',
-
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getStudents(request):
+    students = Student.objects.all()
+    return Response(students)
