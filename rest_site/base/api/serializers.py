e099e627a29c851d9baa15d287d8f170140ef656
Hitesh <hiteshna@atrihub.io>
2025-02-25 10:27:17 -0800

    Delete all files in feature_branch

--- rest_site/base/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from base.models import Student
-
-class StudentSerializer(ModelSerializer):
-    class Meta:
-        model = Student
-        fields = "__all__"
-        
--- rest_site/base/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('allstudents/',views.get_all_students,name='getallStudents'),
-    path('createstudents/',views.create_students,name='createStudents'),
-]
